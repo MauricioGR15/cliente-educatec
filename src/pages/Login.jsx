a7239a5ee567f017303c22c6266e51930c1462b1
Mauricio García <09mgr.15@gmail.com>
2021-05-13 18:49:53 -0600

    Divisison de subcomponentes del Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,8 @@ import CoffeeComputer from "../animations/CoffeeComputer";
 import LoginSvg from "../assets/svg/Login.svg";
 import NavbarLogin from "../components/NavbarLogin";
 import Footer from "../components/Footer";
+import Wrapper from "../layout/Wrapper";
+import Textfield from "../components/Textfield";
 
 const Login = () => {
     useEffect(() => {
@@ -15,73 +17,66 @@ const Login = () => {
     }, []);
 
     return (
-        <div class="w-screen h-screen">
+        <Wrapper>
             <NavbarLogin />
             <main class="w-full h-auto md:h-5/6 flex items-start flex-wrap justify-center">
-                <div class="md:w-1/2 w-full flex flex-col  items-center space-y-16 my-4">
-                    <h3 class="text-violet text-sans text-xl font-semibold">
-                        ¿Eres nuevo en EducaTec?
-                    </h3>
-                    <div id="computer-coffee" className="w-96 h-auto" />
-                    <button
-                        class="h-14 w-40 p-4 bg-violet rounded-full shadow-xl focus:outline-none
-                    transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110 
-                    hover:bg-violet-dark "
-                    >
-                        <Link
-                            to="/registro"
-                            class="text-white font-medium text-lg"
-                        >
-                            Regístrate
-                        </Link>
-                    </button>
-                    <p class="text-center text-blue text-work">
-                        Solo necesitas tu correo
-                        <br />
-                        institucional
-                    </p>
-                </div>
-
-                <div
-                    id="Ingresar"
-                    class=" md:w-1/2 w-full flex flex-col  items-center space-y-16 my-4"
-                >
-                    <h3 class="text-blue text-sans text-xl font-semibold">
-                        Si ya eres parte, ingresa ya!
-                    </h3>
-                    <img class="w-96" src={LoginSvg} />
-                    <form class="w-72 flex flex-col items-center space-y-8">
-                        <input
-                            type="text"
-                            placeholder="No. de Control"
-                            class="text-violet rounded-full shadow-md border-violet h-14 w-full border-2 px-8 outline-none 
-                        transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110 
-                        focus:translate-y-1 focus:scale-110"
-                        />
-                        <input
-                            type="password"
-                            placeholder="Contraseña"
-                            class="text-violet rounded-full shadow-md border-violet h-14 w-full border-2 px-8 outline-none 
-                        transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110 
-                        focus:translate-y-1 focus:scale-110"
-                        />
-                        <button
-                            class="h-14 w-40 p-4 rounded-full bg-blue focus:outline-none shadow-xl
-                    transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110 hover:bg-mint"
-                        >
-                            <Link
-                                to="/home"
-                                class="text-white font-medium text-xl"
-                            >
-                                Ingresar
-                            </Link>
-                        </button>
-                    </form>
-                </div>
+                <SeccionRegistrar />
+                <SeccionIngresar/>
             </main>
-            <Footer/>
-        </div>
+            <Footer />
+        </Wrapper>
     );
 };
+
+const SeccionRegistrar = () => (
+    <div class="md:w-1/2 w-full flex flex-col  items-center space-y-16 my-4">
+        <h3 class="text-violet text-sans text-xl font-semibold">
+            ¿Eres nuevo en EducaTec?
+        </h3>
+        <div id="computer-coffee" className="w-96 h-auto" />
+        <button
+            class="h-14 w-40 p-4 bg-violet rounded-full shadow-xl focus:outline-none
+                    transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110 
+                    hover:bg-violet-dark "
+        >
+            <Link to="/registro" class="text-white font-medium text-lg">
+                Regístrate
+            </Link>
+        </button>
+        <p class="text-center text-blue text-work">
+            Solo necesitas tu correo
+            <br />
+            institucional
+        </p>
+    </div>
+);
+
+const SeccionIngresar = () => (
+    <div
+        id="Ingresar"
+        class=" md:w-1/2 w-full flex flex-col  items-center space-y-16 my-4"
+    >
+        <h3 class="text-blue text-sans text-xl font-semibold">
+            Si ya eres parte, ingresa ya!
+        </h3>
+        <img class="w-96" src={LoginSvg} alt='Imagen referente al login'/>
+        <form class="w-72 flex flex-col items-center space-y-8">
+            <Textfield name='NoControl' type='text'>
+                No. de Control
+            </Textfield>
+            <Textfield name='Password' type='password'>
+                Contraseña
+            </Textfield>
+            <button
+                class="h-14 w-40 p-4 rounded-full bg-blue focus:outline-none shadow-xl
+                    transition duration-500 ease-in-out transform hover:translate-y-1 hover:scale-110 hover:bg-mint"
+            >
+                <Link to="/home" class="text-white font-medium text-xl">
+                    Ingresar
+                </Link>
+            </button>
+        </form>
+    </div>
+);
 
 export default Login;
