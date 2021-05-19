08e8139a05eb67a65c0e7f5171c41146f6a00ebd
Mauricio García <09mgr.15@gmail.com>
2021-05-18 20:38:40 -0600

    Componente PostHome con props

--- src/pages/Home/HomeFeed.jsx
+++ src/pages/Home/HomeFeed.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import PostHome from "../../components/PostHome";
+import MyJson from './posts.json'
 
 const HomeFeed = () => {
-    return (
-        <div>
-            <Navbar/>
-        </div>
-    )
-}
+    const [posts, setPosts] = useState([]);
 
-export default HomeFeed
+    useEffect(() => {
+        const getPosts = async () => {
+            const response = await fetch(MyJson)
+            setPosts(response.json())
+        }
+        getPosts();
+    }, []);
+
+    return <div className="grid grid-cols-1 justify-items-center gap-8"></div>;
+};
+
+export default HomeFeed;
