13a33a00b0526c486df3c38f433dcfc627181c30
Mauricio García <09mgr.15@gmail.com>
2021-05-22 15:58:41 -0600

    Componente modal: modificaciones

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,33 +3,30 @@ import ReactDom from "react-dom";
 
 const Modal = ({ title, isShowing, hide, description, labelAccept }) => (
     <ModalContainer isShowing={isShowing}>
-        <ModalContent>
-            <ModalHeader title={title} hide={hide} />
-            <ModalBody description={description} />
-            <ModalFooter labelAccept={labelAccept} hide={hide} />
-        </ModalContent>
+        <ModalHeader title={title} hide={hide} />
+        <ModalBody description={description} />
+        <ModalFooter labelAccept={labelAccept} hide={hide} />
     </ModalContainer>
 );
 
-export const ModalContainer = ({ isShowing, children }) =>
+export const ModalContainer = ({ isShowing, children }) => (
     isShowing
         ? ReactDom.createPortal(
               <>
                   <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 ">
                       <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                         {children}
+                          <div className="p-4 rounded-lg shadow-md relative flex flex-col h-auto w-full bg-white">
+                              {children}
+                          </div>
                       </div>
                   </div>
-                  <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-              </>
-          , document.body)
-        : null;
-
-export const ModalContent = ({ children }) => (
-    <div className="p-4 rounded-lg shadow-md relative flex flex-col h-auto w-full bg-white ">
-        {children}
-    </div>
-);
+                  <div  className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+              </>,
+              document.body
+          )
+        : null
+)
+    
 
 export const ModalHeader = ({ title, hide }) => (
     <div className="flex items-start justify-between">
@@ -54,7 +51,7 @@ export const ModalBody = ({ description, children }) => (
     </div>
 );
 
-export const ModalFooter = ({ hide, labelAccept, onAccept }) => (
+export const ModalFooter = ({ hide, labelAccept }) => (
     <div className="flex items-center justify-end  ">
         <button
             className="text-mint-dark font-work background-transparent font-semibold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -65,10 +62,9 @@ export const ModalFooter = ({ hide, labelAccept, onAccept }) => (
         </button>
         <button
             className="bg-violet text-white font-work active:bg-violet-dark font-semibold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="button"
+            type="submit"
             data-dismiss="modal"
-            aria-label="Cerrar"
-            onClick={hide}
+            aria-label={labelAccept}
         >
             {labelAccept}
         </button>
